30c35b1f90021c239e6a472402cc976dd16d67b2
rosangelabsilva <rosangela-bsilva@hotmail.com>
2019-10-22 13:32:44 -0300

    function password confirm

--- src/pages/register.js
+++ src/pages/register.js
@@ -8,7 +8,7 @@ function btnRegister() {
   let name = document.querySelector('.js-name-register').value;
   let lastname = document.querySelector('.js-lastname-register').value;
   let birthday = document.querySelector('.js-birthday-register').value;
-  firebase.auth().createUserWithEmailAndPassword(email, password).then(function () {
+  firebase.auth().createUserWithEmailAndPassword(email, password).then(() => {
     user = firebase.auth().currentUser;
     if (user != null) {
       uid = user.uid;
@@ -44,6 +44,8 @@ function Register() {
       ${Input({ type: 'email', class: 'js-email-confirm', id: 'email-confirm', onBlur: confirmEmail, placeholder: 'Digite seu e-mail novamente' })}
       <p id='aviso'></p>
       ${Input({ type: 'password', class: 'js-password-register', onBlur: nullFunction, placeholder: 'Digite a senha' })}
+      ${Input({ type: 'password', class: 'js-password-confirm', onBlur: confirmPassword, placeholder: 'Digite a senha novamente' })}
+
       ${Input({ type: 'file', class: 'my-file'})}
       </form>
       <form class="btnregister">
@@ -62,12 +64,30 @@ function Register() {
 function confirmEmail() {
   let email = document.querySelector('.js-email-register').value;
   let emailConfirm = document.querySelector('.js-email-confirm').value;
+  const buttonRegister = document.querySelector('#btnRegister');
   if (email !== emailConfirm) {
     document.querySelector('#aviso').innerHTML = 'E-mails não coincidem';
-    document.querySelector('#btnRegister').disabled = true;
+    buttonRegister.disabled = true;
+    buttonRegister.style.backgroundColor = '#808080';
   } else {
     document.querySelector('#aviso').innerHTML = '';
-    document.querySelector('#btnRegister').disabled = false;
+    buttonRegister.disabled = false;
+    buttonRegister.style.backgroundColor = '#F76900';
+  }
+}
+
+function confirmPassword() {
+  let password = document.querySelector('.js-password-register').value;
+  let passwordConfirm = document.querySelector('.js-password-confirm').value;
+  const buttonRegister = document.querySelector('#btnRegister');
+  if (password !== passwordConfirm) {
+    document.querySelector('#aviso').innerHTML = 'Senhas não coincidem';
+    buttonRegister.disabled = true;
+    buttonRegister.style.backgroundColor = '#808080';
+  } else {
+    document.querySelector('#aviso').innerHTML = '';
+    buttonRegister.disabled = false;
+    buttonRegister.style.backgroundColor = '#F76900';
   }
 }
 
